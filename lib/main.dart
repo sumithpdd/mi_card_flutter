09f78d878be00036efac6161973ab84bd7b1e8a8
sumith damodaran <sumithpd@gmail.com>
2019-05-05 03:01:06 +0200

    Flutter layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.greenAccent[200],
+        backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
+            child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-                width: 100,
-                height: 100,
-                color: Colors.white,
-                child: Text('Container 1')),
-            SizedBox(
-              height: 20,
+                width: 100, color: Colors.red, child: Text('Container 1')),
+            Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 210,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                      child: Text('Container 2'),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                      child: Text('Container 3'),
+                    ),
+                  ]),
             ),
             Container(
               width: 100,
-              height: 100,
               color: Colors.blue,
-              child: Text('Container 2'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.red,
-              child: Text('Container 3'),
+              child: Text('Container 4'),
             ),
           ],
         )),
