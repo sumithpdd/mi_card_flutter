5c00c02f38a5a614828242ed1238e68b3e396196
sumith damodaran <sumithpd@gmail.com>
2019-05-05 02:39:30 +0200

    columnwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.greenAccent[200],
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.white,
+                child: Text('Container 1')),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Text('Container 3'),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
