e2ffd057e917434c77407a02b944620fcd92a5a6
sumith damodaran <sumithpd@gmail.com>
2019-05-05 04:03:09 +0200

    Business card

--- lib/main.dart
+++ lib/main.dart
@@ -11,39 +11,93 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-                width: 100, color: Colors.red, child: Text('Container 1')),
-            Container(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage:
+                      AssetImage('images/sumithsitecoreplane.jpg')),
+              Text(
+                'Sumith Parambat Damodaran',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Sitecore Authority',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    color: Colors.teal[100],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25),
+                child: ListTile(
+                    leading: Icon(Icons.web_asset, color: Colors.teal),
+                    title: Text(
+                      'www.sumithpd.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20),
+                    )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(children: <Widget>[
+                    Icon(Icons.phone, color: Colors.teal),
                     SizedBox(
-                      width: 210,
+                      width: 10,
                     ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.yellow,
-                      child: Text('Container 2'),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.green,
-                      child: Text('Container 3'),
-                    ),
+                    Text(
+                      '+44 7894521878',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20),
+                    )
                   ]),
-            ),
-            Container(
-              width: 100,
-              color: Colors.blue,
-              child: Text('Container 4'),
-            ),
-          ],
-        )),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(children: <Widget>[
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'sumithpd@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20),
+                    )
+                  ]),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
